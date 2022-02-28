42ee510e55508f7e1fe1979c18c5d71cc4371d16
Mak-43 <letsmailmak@gmail.com>
2022-03-01 00:14:25 +0600

    phone details added

--- JS File/app.js
+++ JS File/app.js
@@ -1,3 +1,4 @@
+// search and fetch all phones
 const loadGadget=()=>{
     const searchText=document.getElementById('user-input').value;
 
@@ -8,6 +9,7 @@ const loadGadget=()=>{
     .then(data=>displayPhone(data.data))
     
 }
+// display phones 
 const displayPhone=(phones)=>{
     const container=document.getElementById('container');
     const first20Data=phones.slice(0,20)
@@ -31,12 +33,47 @@ const displayPhone=(phones)=>{
          container.appendChild(div)
     }
 }
+// fetch phone details
 const phoneDetails=(details)=>{
     const url=`https://openapi.programming-hero.com/api/phone/${details}`
     fetch(url)
     .then(res=>res.json())
-    .then(data=>displayDetails(data))
+    .then(data=>displayDetails(data.data))
 }
+// display phone details
 const displayDetails=(d)=>{
     console.log(d)
+    const modal=document.getElementById('modal')
+    
+        const div=document.createElement('div')
+        div.innerHTML=`
+        
+       
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header text-center">
+              <img src="${d.image}" alt="">
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h5 class="card-title">${d.name}</h5>
+                <p>${d.releaseDate}</p>
+                <p>${d.mainFeatures.memory}</p>
+                <p>${d.mainFeatures.chipSet}</p>
+                <p>${d.mainFeatures.displaySize}</p>
+                <p>sensors</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+     
+        
+        `
+        
+    modal.appendChild(div)
+
+    
 }
