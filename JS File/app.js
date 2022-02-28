57de9e5566404f02684034e3ecd0fcb73ceaee56
Mak-43 <letsmailmak@gmail.com>
2022-03-01 01:55:28 +0600

    phn details

--- JS File/app.js
+++ JS File/app.js
@@ -6,12 +6,15 @@ const loadGadget=()=>{
 
     fetch(url)
     .then(res=>res.json())
-    .then(data=>displayPhone(data.data))
     
+    .then(data=>displayPhone(data.data))
+   
 }
 // display phones 
 const displayPhone=(phones)=>{
+    
     const container=document.getElementById('container');
+    
     const first20Data=phones.slice(0,20)
     for(const phone of first20Data){
         const div=document.createElement('div')
@@ -19,8 +22,8 @@ const displayPhone=(phones)=>{
         div.className="col-md-4 col-12"
         div.innerHTML=`
          
-             <div class="card m-2 rounded-3" style="width: 18rem;">
-                 <img src="${phone.image}" class="card-img-top img-fluid p-3 rounded" alt="...">
+             <div class="card shadow p-3 mb-5 bg-body rounded-3" style="width: 18rem;">
+                 <img src="${phone.image}" class="card-img-top shadow-sm p-3 mb-5 bg-body rounded" alt="...">
                  <div class="card-body">
                      <h5 class="card-title">${phone.phone_name}</h5>
                      <h6 class="card-title">${phone.brand}</h6>
@@ -41,39 +44,33 @@ const phoneDetails=(details)=>{
     .then(data=>displayDetails(data.data))
 }
 // display phone details
-const displayDetails=(d)=>{
-    console.log(d)
-    const modal=document.getElementById('modal')
-    
-        const div=document.createElement('div')
-        div.innerHTML=`
-        
+const displayDetails=(d)=>{  
+    const modal=document.getElementById('modal')    
+        const div=document.createElement('div')    
+        div.innerHTML=`          
+            <div class="modal-dialog">
+                <div class="modal-content shadow p-3 mb-5 bg-body rounded-3">
+                    <div class="modal-header text-center mx-auto">
+                        <img src="${d.image}" alt="">
+                    </div>
+                    <div class="modal-body">
+                        <h5 class="card-title">${d.name}</h5>
+                        <p>${d.releaseDate}</p>
+                        <p>Storage: ${d.mainFeatures.memory}</p>
+                        <p>Chipset: ${d.mainFeatures.chipSet}</p>
+                        <p>Displaysize: ${d.mainFeatures.displaySize}</p>
+                        
+                        <p>Sensors: ${d.mainFeatures.sensors}</p>
+                        
+                    </div>
+                    <div class="modal-footer mx-auto">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>  
+       `  
        
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header text-center">
-              <img src="${d.image}" alt="">
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <h5 class="card-title">${d.name}</h5>
-                <p>${d.releaseDate}</p>
-                <p>${d.mainFeatures.memory}</p>
-                <p>${d.mainFeatures.chipSet}</p>
-                <p>${d.mainFeatures.displaySize}</p>
-                <p>sensors</p>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-     
-        
-        `
-        
-    modal.appendChild(div)
-
-    
+       
+    modal.appendChild(div) 
 }
